28610cf9307a5ceecd9284d382616c01442e2624
gurami <guramidgebuadze@gmail.com>
2023-01-15 17:54:05 +0400

    modal after order now has info what was bought and grand total price

--- assets/js/modalAfterOrder.js
+++ assets/js/modalAfterOrder.js
@@ -1,21 +1,21 @@
-import { addChosenItemInfoToCart } from "./addToCartBtn.js";
+import { chosenProductImage } from "./imagesPaths.js";
 
 // Create modal after order
 const continueAndPayBtn = document.querySelector(".continue-and-pay");
 const header = document.querySelector(".header-wrapper");
-const notificationBtn = document.querySelector("#notifications-btn");
+const grandTotal = document.querySelector(
+  ".checkout-form-summary-prices-grand-total"
+);
 
 export function createModalAfterOrder() {
   continueAndPayBtn?.addEventListener("click", () => {
-    localStorage.removeItem("products");
+    const chosenItemsInfo = JSON.parse(localStorage.getItem("products"));
 
-    // Clear chosen items amount
-    addChosenItemInfoToCart();
-    notificationBtn?.classList.remove("empty");
-
-    const modalAfterOrder = document.createElement("div");
-    modalAfterOrder.className = "bought-product-info-wrapper";
-    modalAfterOrder.innerHTML = `
+    // Check if items are chosen
+    if (chosenItemsInfo?.length) {
+      const modalAfterOrder = document.createElement("div");
+      modalAfterOrder.className = "bought-product-info-wrapper";
+      modalAfterOrder.innerHTML = `
     <div class="bought-product-info">
     <div id="tick-mark-wrapper"> 
       <div id="tick-mark"> </div> 
@@ -26,39 +26,38 @@ export function createModalAfterOrder() {
     <div class="bought-products">
       <div class="bought-products-list checkout-form-summary-list">
         <ul>
-          <li>
-            <img
-              src="../assets/images/product-xx59-headphones/mobile/image-product.jpg"
-            />
-            <div class="checkout-form-summary-list-col2">
-              <p class="checkout-form-summary-list-product-name">
-                XX99 MK II
-              </p>
-              <p
-                class="checkout-form-summary-list-product-price"
-              >
-                $ 2,999
-              </p>
-            </div>
-            <div class="checkout-form-summary-list-col3">
-              <p
-                class="checkout-form-summary-list-product-number"
-              >
-                x4
-              </p>
-            </div>
-          </li>
+        ${chosenItemsInfo
+          ?.map(
+            ({ price, quantity, productName }) =>
+              `<li>
+              <img src=${chosenProductImage(productName)} alt="${productName}">
+              <div class="checkout-form-summary-list-col2">
+                <p class="checkout-form-summary-list-product-name">
+                ${productName.replaceAll("-", "")}
+                </p>
+                <p class="checkout-form-summary-list-product-price">$ ${price
+                  .toString()
+                  .replace(/\B(?=(\d{3})+(?!\d))/g, ",")}</p>
+              </div>
+              <div class="checkout-form-summary-list-col3">
+                <p class="checkout-form-summary-list-product-number">x${quantity}</p>
+              </div>
+            </li>`
+          )
+          .join("")}
         </ul>
       </div>
     <div class="total-spent-money">
       <h3>GRAND TOTAL</h3>
-      <p>$ 5,446</p>
+      <p>$ ${grandTotal.textContent}</p>
     </div>
     </div
     <button><a href="../index.html" class="button-primary back-to-home-btn" >BACK TO HOME</a></button>
 </div>
 `;
 
-    header.appendChild(modalAfterOrder);
+      header.appendChild(modalAfterOrder);
+      localStorage.removeItem("products");
+    }
   });
 }
